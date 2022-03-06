43148cb773833841d6ad39a15fde30f06ba6b3e9
Matheus Alvarenga França <matheus.alvfr@gmail.com>
2022-03-06 16:36:53 -0300

    Create Future boilerplate

--- lib/bex_web/endpoint.ex
+++ lib/bex_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule BexWeb.Endpoint do
   ]
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
-
+  socket "/socket", BexWeb.UserSocket, websocket: true, longpoll: false
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
