25f6591587183ef673608ec96135c9046059f273
Matheus Alvarenga França <matheus.alvfr@gmail.com>
2022-03-17 00:05:51 -0300

    Monitoring OS data

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Bex.MixProject do
   def application do
     [
       mod: {Bex.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
